04496e9c72d651460d98812f1c532e4f1dc6af2f
John Kim <johnk9000@gmail.com>
2020-07-01 12:20:12 -0500

    Minor updates

--- logic.js
+++ logic.js
@@ -2,6 +2,9 @@ let albumArt;
 let artistName = '2pac';
 var qURL = 'http://api.deezer.com/search?q=' + artistName;
 
+//album art retreival algorithm
+function artistAlbum() {
+// Find related albums to artist (single item) and then push it into an array
 $.ajax({
     url: qURL,
     method: 'GET'
@@ -10,15 +13,10 @@ $.ajax({
         var artist = artist.nameval();
             console.log(name);
         var album = album.title.val();
-        var albumURL = album.cover_bigval();
+        var albumURL = album.cover_big.val();
 
         albumArt.push(album, albumURL);
     });
     console.log(albumArt);
 });
-
-//album art retreival algorithm
-function artistAlbum() {
-// Find related albums to artist (single item) and then push it into an array
-
 };
