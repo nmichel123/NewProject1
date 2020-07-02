7cb1f30ce6d651c7b7b196521fb2105e0dcbdd0f
John Kim <johnk9000@gmail.com>
2020-07-02 18:14:49 -0500

    Working video gen engine. We've acheived MVP

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-$(document).on("click", "#search-button", function(e){
-    e.preventDefault();
-   
-    var artGallery = $('#art');
-    artGallery.empty();
-    var artHead = $('<div class="bins aside-head">');
-    artGallery.append(artHead);
-    var hTag = $('<h2>');
-    hTag.text("Album Artwork");
-    
-    let albumArt;
-    let artistName = $("#search-input").val();
+function artGen(artistName) {
     var queryUrl = "https://api.discogs.com/database/search?q=" + artistName +"&key=RgHfTojdnkeQgCqFmkTT&secret=twfdmVrizBUNTUHHonIfYakTJsQaQPbO";
     //album art retreival algorithm
     // Find related albums to artist (single item) and then push it into an array
@@ -17,6 +6,9 @@ $(document).on("click", "#search-button", function(e){
         url: queryUrl,
         method: 'GET'
     }).then(function(response) {
+        var artGallery = $('#art');
+        artGallery.empty();;
+        
         let len = 24;
         for(i = 0; i < len; i++){
             var album = response.results[i + 1].title;
@@ -26,37 +18,38 @@ $(document).on("click", "#search-button", function(e){
                 //console.log(i);
             }
             var albumURL = response.results[i + 1].cover_image;
-            //console.log(albumURL)
-            //albumArt.push(album);
-
-            //console.log(albumArt);
-        var imgGen = $('<img>');
-        imgGen.addClass('artwork');
-        imgGen.attr("style", "max-width: 250px; border: 5px solid grey; flex-wrap: wrap;");
-        imgGen.attr('id', 'album-art-'+ i)
-        imgGen.attr('src', albumURL);
-        artGallery.append(imgGen);
-    
-    }
+            var imgGen = $('<img>');
+            imgGen.addClass('artwork');
+            imgGen.attr("style", "max-width: 250px; border: 5px solid grey; flex-wrap: wrap;");
+            imgGen.attr('id', 'album-art-'+ i)
+            imgGen.attr('src', albumURL);
+            artGallery.append(imgGen);
+        }
     });
+}
+
+function vidGen(artistName) {
+    // Remove previous contents to make room for new content
+    $('#videos').empty();
 
     var ytKey = 'AIzaSyDU8dDA7emi_uv9R_LXIIu2jprRZJ6-wR8';
-    var ytURL = 'https://www.googleapis.com/youtube/v3/search?q=' + artistName + "&type=video&maxResults=12&part=snippet&key=" + ytKey;
+    var ytURL = 'https://www.googleapis.com/youtube/v3/search?q=' + artistName + "+concert&type=video&maxResults=12&part=snippet&key=" + ytKey;
     $.ajax({
         url: ytURL,
         method: 'GET'
     }).then(function(response) {
         console.log(response);
         var videosYT = response.items;
-
+            console.log(videosYT);
         for(i = 0; i < videosYT.length; i++){
-            
+            var iframeGen = $('<iframe width="560" height="315" allowfullscreen>');
+            iframeGen.attr('src', 'https://www.youtube.com/embed/' + videosYT[i].id.videoId);            
+            $('#videos').append(iframeGen);
         }
     });
-        
+}
 
-        
-// Download button generators
+function downloadGen(videoUrl) {
     $('#Qual').empty();
     let quality = ['mp3 (audio)', '720p', '1080p', '4k'];
     console.log(quality);
@@ -75,10 +68,20 @@ $(document).on("click", "#search-button", function(e){
         console.log(quality[i]);
     $('#row-' + i).append(hTag);
 
-    var btnGen = $('<button>');
+    var btnGen = $('<button class="btn"><i class="fa fa-download"></i>');
     btnGen.text('download');
-    btnGen.attr('link', albumURL)
+    btnGen.attr('link', videoUrl);
     $('#row-' + i).append(btnGen);
     }
-    
+}
+
+$(document).on("click", "#search-button", function(e) {
+    e.preventDefault();
+    artistName = $("#search-input").val();
+
+    artGen(artistName);
+    vidGen(artistName);  
 });
+
+artGen('rick roll');
+vidGen('Rick-Roll');
