c97617eef72fc3d1eee8da2819fde6f0ba2066e2
John Kim <johnk9000@gmail.com>
2020-07-02 11:13:42 -0500

    Got album covers to render

--- script.js
+++ script.js
@@ -1,50 +1,39 @@
-let art; //var art = ['src=url1', 'src=url2', 'src=url3', ... ];
-let video; //var video = ['src=url1', 'src=url2', 'src=url3', ... ];
-
-function renderArt(){
-    artistAlbum()
-
-    let len = albumArt.length;
-    if( albumArt.length > 24) {
-        len = 20;
-    }
-    var artGallery = $('.table');
-
+$(document).on("click", "#search-button", function(e){
+   
+   
+    var artGallery = $('#art');
     artGallery.empty();
     var artHead = $('<div class="bins aside-head">');
     artGallery.append(artHead);
-
     var hTag = $('<h2>');
     hTag.text("Album Artwork");
-
-    for(i = 0; i < len; i ++) {
-        console.log(albumArt);
-        var imgGen = $('<img>');
-        imgGen.addClass('');
-        imgGen.attr("style", "width: 15%; border: 5px solid var(--hue-background); flex-wrap: wrap;");
-        imgGen.attr('id', 'album-art-'+ i)
-        imgGen.attr('src', albumArt.albumURL[i]);
-        artGallery.append(imgGen);
-    }
-           
-    art.forEach(element => {
-        var iframeGen = $('<iframe>');
-            iframeGen.attr('id', 'video-' + i);
-            var entry = video.url;
-            iframeGen.attr('src', entry);
-                console.log(entry);
-            $('#album-art-' + i).append(iframeGen);
-    });       
-
-            
-};
-
-$('#search').button.on("click", function(e){
-    renderBins();
+    
+        let albumArt;
+        let artistName = $("#search-input").val();
+        var queryUrl = "https://api.discogs.com/database/search?q=" + artistName +"&key=RgHfTojdnkeQgCqFmkTT&secret=twfdmVrizBUNTUHHonIfYakTJsQaQPbO";
+        //album art retreival algorithm
+        // Find related albums to artist (single item) and then push it into an array
+        $.ajax({
+            url: queryUrl,
+            method: 'GET'
+        }).then(function(response) {
+            console.log(response.results[5].cover_image);
+            for(i=0; i<12; i++){
+                var album = response.results[i + 1].title;
+                if(album == response.results[i+2].title){ //if statement to advance by 1 if next album is a repeat
+                    i++;
+                }
+                var albumURL = response.results[i + 1].cover_image;
+                console.log(albumURL)
+                //albumArt.push(album);
+    
+                console.log(albumArt);
+            var imgGen = $('<img>');
+            imgGen.addClass('');
+            imgGen.attr("style", "max-width: 250px; border: 5px solid var(--hue-background); flex-wrap: wrap;");
+            imgGen.attr('id', 'album-art-'+ i)
+            imgGen.attr('src', albumURL);
+            artGallery.append(imgGen);
+            }
+        });
 });
-
-$('.bins').on('click', function(e) {
-
-});
-
-renderBins();
